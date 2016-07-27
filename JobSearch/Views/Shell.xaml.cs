3c9729759048370569900b4a41b22dd3f937ee2b
armando-n <armando.n@outlook.com>
2016-07-27 07:49:10 -0500

    Cleaned up UI; Jobs btn navigates now

--- JobSearch/Views/Shell.xaml.cs
+++ JobSearch/Views/Shell.xaml.cs
@@ -32,17 +32,17 @@ namespace JobSearch.Views
             MyHamburgerMenu.NavigationService = navigationService;
         }
 
-        private void Accounts_Tapped(object sender, RoutedEventArgs e)
-        {
-            Account1Button.Visibility = Account2Button.Visibility =
-                (Account1Button.Visibility == Visibility.Visible) ? Visibility.Collapsed : Visibility.Visible;
-        }
+        //private void Accounts_Tapped(object sender, RoutedEventArgs e)
+        //{
+        //    Account1Button.Visibility = Account2Button.Visibility =
+        //        (Account1Button.Visibility == Visibility.Visible) ? Visibility.Collapsed : Visibility.Visible;
+        //}
 
-        private void Folders_Tapped(object sender, RoutedEventArgs e)
-        {
-            b1.Visibility = b2.Visibility = b3.Visibility = b4.Visibility = b5.Visibility = b6.Visibility = b7.Visibility =
-                (b1.Visibility == Visibility.Visible) ? Visibility.Collapsed : Visibility.Visible;
-        }
+        //private void Folders_Tapped(object sender, RoutedEventArgs e)
+        //{
+        //    b1.Visibility = b2.Visibility = b3.Visibility = b4.Visibility = b5.Visibility = b6.Visibility = b7.Visibility =
+        //        (b1.Visibility == Visibility.Visible) ? Visibility.Collapsed : Visibility.Visible;
+        //}
 
         private void Add_Tapped(object sender, RoutedEventArgs e)
         {
